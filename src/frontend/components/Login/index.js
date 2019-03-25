5b75a8e042948348499fc61c9067b2f7b4b3f4f9
Jakub Niewczas <niewczas.jakub@gmail.com>
2019-03-25 09:16:47 +0900

    persist user email in cookie

--- src/frontend/components/Login/index.js
+++ src/frontend/components/Login/index.js
@@ -1,9 +1,12 @@
+import Cookies from 'js-cookie';
+
 import connect from 'lib/appState/connect';
 
 import Component from './component';
 
 const mapStateToProps = (_state, _props, updateState) => ({
   onLogin: (userEmail) => {
+    Cookies.set('email', userEmail);
     updateState({
       userEmail,
     });
--- src/frontend/index.jsx
+++ src/frontend/index.jsx
@@ -1,14 +1,20 @@
 import 'es6-promise/auto';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Cookies from 'js-cookie';
 import { StateProvider } from 'lib/appState';
 
 import App from 'components/App';
 
 import 'assets';
 
+const userEmail = Cookies.get('email');
+const initialState = {
+  userEmail,
+};
+
 ReactDOM.render(
-  <StateProvider initialState={{}}>
+  <StateProvider initialState={initialState}>
     <App />
   </StateProvider>,
   document.getElementById('app'),
