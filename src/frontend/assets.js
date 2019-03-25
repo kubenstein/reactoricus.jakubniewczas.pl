a9645f5a05e168a49f824f33635de7c44db0f1fc
Jakub Niewczas <niewczas.jakub@gmail.com>
2019-03-25 09:02:27 +0900

    add favicon

---
+++ src/frontend/assets.js
@@ -0,0 +1,3 @@
+import 'index.html';
+import 'assets/stylesheets/styles.scss';
+import 'assets/images/favicon.png';
--- src/frontend/index.jsx
+++ src/frontend/index.jsx
@@ -1,12 +1,10 @@
 import 'es6-promise/auto';
-
-import 'index.html';
-import 'assets/stylesheets/styles.scss';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from 'components/App';
+
+import 'assets';
 
 ReactDOM.render(
   <App />,
