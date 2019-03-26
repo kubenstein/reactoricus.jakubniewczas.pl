7684915900e9f4d76bd1a7c0578b354128d2065d
Jakub Niewczas <niewczas.jakub@gmail.com>
2019-03-26 09:30:35 +0900

    convert function to shorter return syntax

--- src/frontend/components/Maps/component.jsx
+++ src/frontend/components/Maps/component.jsx
@@ -6,27 +6,25 @@ import Map from 'components/Map';
 import Footer from 'components/Footer';
 import './styles.css';
 
-const Maps = ({ maps }) => {
-  return (
-    <>
-      <MapHeader />
-      <div styleName="wrapper">
-        <p>
-          <strong>Lead the Reactoricus </strong>
-          is a game where, by using movement blocks, you can guide a tiny robot.
-          The goal is to collect all stars on each map.
-        </p>
-        <span styleName="separator">Choose the map you want to play on!</span>
-        <div styleName="maps">
-          {maps.map(map => (
-            <Map key={map.id} map={map} />
-          ))}
-        </div>
+const Maps = ({ maps }) => (
+  <>
+    <MapHeader />
+    <div styleName="wrapper">
+      <p>
+        <strong>Lead the Reactoricus </strong>
+        is a game where, by using movement blocks, you can guide a tiny robot.
+        The goal is to collect all stars on each map.
+      </p>
+      <span styleName="separator">Choose the map you want to play on!</span>
+      <div styleName="maps">
+        {maps.map(map => (
+          <Map key={map.id} map={map} />
+        ))}
       </div>
-      <Footer />
-    </>
-  );
-};
+    </div>
+    <Footer />
+  </>
+);
 
 Maps.propTypes = {
   maps: PropTypes.arrayOf(PropTypes.any), // add map shape
