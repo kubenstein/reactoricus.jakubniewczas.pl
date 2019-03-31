245811d2889715c6ee1daa20ddadaa25e25e891c
Jakub Niewczas <niewczas.jakub@gmail.com>
2019-03-31 22:19:59 +0900

    camera follows robot

---
+++ unity_game/Assets/Prefabs/Camera/CameraMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+    public float speed = 1;
+    private GameObject target;
+    private Vector3 offset;
+
+    void Update() {
+        if (!target) {
+            target = GameObject.FindGameObjectsWithTag("reaktoricus")[0];
+            offset = transform.position - target.transform.position;
+        }
+
+        transform.position = Vector3.Lerp(
+            transform.position,
+            target.transform.position + offset,
+            Time.deltaTime * speed);
+    }
+}
