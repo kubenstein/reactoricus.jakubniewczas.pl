d8fe477ba7796fe2aa0d70fa19a1ce22ab4368ed
Jakub Niewczas <niewczas.jakub@gmail.com>
2019-03-31 22:08:16 +0900

    change to nice looking assets

---
+++ unity_game/Assets/Prefabs/Map/SegmentStar/SegmentStarRotate.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SegmentStarRotate : MonoBehaviour {
+    public float speed = 2;
+
+    private void FixedUpdate() {
+        transform.Rotate(new Vector3(0, 1, 0) * speed);
+    }
+}
---
+++ unity_game/Assets/Prefabs/Reaktoricus/ReaktoriousHovering.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReaktoriousHovering : MonoBehaviour {
+    public float speed = 0.1f;
+    public float amplitude = 0.1f;
+    public float initialY;
+    private bool up = true;
+
+    void Awake() {
+        initialY = transform.position.y;
+    }
+
+    void Update() {
+        Vector3 position = transform.position;
+        if (position.y > initialY + amplitude) up = !up;
+        if (position.y < initialY-amplitude) up = !up;
+        position.y += Time.deltaTime * speed * (up ? 1 : -1);
+        transform.position = position;
+    }
+}
