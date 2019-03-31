58725a95278ef052e5acb396bdcc3507f841b07f
Jakub Niewczas <niewczas.jakub@gmail.com>
2019-03-31 19:53:51 +0900

    send event about collecting all stars

--- unity_game/Assets/Prefabs/Reaktoricus/ReaktoricusCollectStar.cs
+++ unity_game/Assets/Prefabs/Reaktoricus/ReaktoricusCollectStar.cs
@@ -1,11 +1,28 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ReaktoricusCollectStar : MonoBehaviour {
+    private int starsCount = 0;
+
+    private void Awake() {
+        starsCount = GameObject.FindGameObjectsWithTag("star").Length;
+    }
+
     private void OnTriggerEnter(Collider other) {
         if (other.tag.Equals("star")) {
             Destroy(other.gameObject);
+            starsCount -= 1;
+            if (starsCount == 0) {
+                FinishGame();
+            }
         }
+    }
+
+    void FinishGame() {
+        try {
+            WebBinding.OnEvent("GameFinished");
+        } catch (Exception e) { }
     }
 }
