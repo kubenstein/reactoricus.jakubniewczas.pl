0ed4973e584864335e318efefc73c6ee6161f16e
Jakub Niewczas <niewczas.jakub@gmail.com>
2019-04-27 17:54:59 +0900

    add coordinates grammar validation

--- src/backend/add-map/validate/index.js
+++ src/backend/add-map/validate/index.js
@@ -1,10 +1,18 @@
 import ValidationResult from './validation-result';
 
+const validCoordinatesGrammar = string => (
+  (string || '')
+    .split('|')
+    .map(box => /^(-?\d*),(-?\d*),[01]$/.test(box))
+    .filter(valid => !valid)
+    .length === 0
+);
+
 export default function validate({ coordinates = '', name = '' }) {
   const result = new ValidationResult();
   if (name.length < 1 || name.length > 60) result.addError('Name cant be empty but also has to be up to 60 chars long');
   if (!/^[a-zA-Z0-9 ]+$/.test(name)) result.addError('Name can include only letters numbers and space');
   if (coordinates.length < 1) result.addError('Coordinates cant be blank');
-  // Todo: check if coordinates are valid and if the lvl is solvable
+  if (!validCoordinatesGrammar(coordinates)) result.addError('Coordinates has to follow "<x>,<y>,<withBadge [1,0]|..." pattern');
   return result;
 }
